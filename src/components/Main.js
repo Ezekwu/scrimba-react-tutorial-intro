9c4b06b48fe7fa7b93cecd1017b8d5aca39defeb
jeremiah <100346840+Ezekwu@users.noreply.github.com>
2022-06-15 10:36:24 -0700

    update

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,66 @@
+import { useState } from "react"
 
 
 const Main = () => {
+    
+    const [formData, setFormData] = useState({
+        username: "",
+        password: "",
+        passwordConfirmation: "",
+        
+    })
+    const handleChange =  (e) => {
+        const {name, value, checked} = e.target;
+        setFormData(prevData => {
+            return  {
+                ...prevData,
+                [name]: value
+            }
+            
+        })
+        console.log(formData);
+    }
+    const submit =(e) => {
+        e.preventDefault()
+        console.log(formData);
+    }
     return (
         <section className="main">
-            <h1>Fun facts about React</h1>
-            <div className="list">
-                <ul>
-                    <li>Was first released in 2023</li>
-                    <li>Was originally created by Jordan Walke</li>
-                    <li>Has well over 100k stars on GitHub</li>
-                    <li>Is maintained by Facebook</li>
-                    <li>Powers thousands of enterprise apps, including mobile apps</li>
-                </ul>
+            <form >
+                <input type="text"
+                placeholder="username"
+                name="username"
+                value={formData.username}
+                onChange={handleChange}/>
+
+                <input type="password"
+                placeholder="password" 
+                name="password"
+                value={formData.password}
+                onChange={handleChange}/>
+
+                <input type="text"  
+                placeholder="confirm password"
+                name="passwordConfirmation"
+                value={formData.passwordConfirmation}
+                onChange={handleChange}
+                />
+
+                <div className="check-container">
+                    <input type="checkbox" 
+                    id="check"/>
+                    <label htmlFor="check">join news letter</label>
+                </div>
+                <button>sign up</button>
+            </form>
+            {/* <div className="form">
+                <div className="input">
+                    <input type="text" placeholder="Top text" className="input-1" />
+                    <input type="text" placeholder="Bottom text" className="input-2"[i] />
+                </div>
+                <button >Get a new meme image  🖼</button>
             </div>
+            <img src="../images/memeimg.png" alt="" className="main-image"/> */}
         </section>
     )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,15 @@
-import logo from '../logo.svg';
+
 
 
 const Navbar = () => {
     return (
         <nav>
             <div className="logo-header">
-                <img src={logo} alt="" />
-                <h3>ReactFacts</h3>
+                <img src="../images/Troll Face.png" alt="" />
+                <h3>Meme Generator</h3>
             </div>
             <div className="nav-item">
-                <h4>React Course- project 1</h4>
+                <h4>React Course- project 3</h4>
             </div>
         </nav>
     )
